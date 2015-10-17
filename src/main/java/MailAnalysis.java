ec8ecef5cf9e24f500052fd99596df7b8c40740f
ChikilevMax <dom_prodakt@ukr.net>
2015-10-17 20:57:59 +0300

    Final version

--- src/main/java/MailAnalysis.java
+++ src/main/java/MailAnalysis.java
@@ -13,7 +13,7 @@ public class MailAnalysis {
     private static final int NUMBER_OF_LETERS = 30;
     private List<String> senderName = new ArrayList<String>();
     private List<SenderCounter> senderCounters = new ArrayList<SenderCounter>();
-    private int mailCounter = 0;
+    private int mailCounter;
 
 
     public MailAnalysis(WebDriver webDriver) {
@@ -21,13 +21,12 @@ public class MailAnalysis {
     }
 
     private void loadMailProgram() {
-        String pass;
         webDriver.get("https://mail.ukr.net/classic/login");
         WebElement login = findWithExplicitWait(15, By.id("login"));
         login.sendKeys("dom_prodakt@ukr.net");
         WebElement password = webDriver.findElement(By.id("password"));
         Scanner in = new Scanner(System.in);
-        pass = in.nextLine();
+        String pass = in.nextLine();
         password.sendKeys(pass);
         WebElement loginButton = webDriver.findElement(By.xpath("//*[@id=\"login-form\"]/div[3]/button"));
         loginButton.click();
@@ -41,13 +40,13 @@ public class MailAnalysis {
         while (mailCounter <= NUMBER_OF_LETERS) {
             List<WebElement> mailList = webDriver.findElements(By.xpath("//*[starts-with(@class, 'message unread')]"));
             mailCounter = mailCounter + mailList.size();
-            parsingElementList(mailList);
+            parseElementList(mailList);
             WebElement nextPageButton = webDriver.findElement(By.xpath("//*[@id=\"msglist-page\"]/div[4]/div/a[3]"));
             nextPageButton.click();
         }
     }
 
-    private void parsingElementList(List<WebElement> webElement) {
+    private void parseElementList(List<WebElement> webElement) {
 
         for (WebElement aWebElement : webElement) {
             addSenderCounter(aWebElement);
@@ -56,11 +55,11 @@ public class MailAnalysis {
 
     private void addSenderCounter(WebElement currentWebElement) {
         int indexSenderName;
-        String nameSender;
-        String mailSubject;
         SenderCounter senderCounter;
-        nameSender = currentWebElement.findElement(By.className("from-name")).getText();
-        mailSubject = currentWebElement.findElement(By.className("subject-text")).getText();
+        WebElement nameSenderElement = currentWebElement.findElement(By.className("from-name"));
+        String nameSender = nameSenderElement.getText();
+        WebElement mailSubjectElement = currentWebElement.findElement(By.className("subject-text"));
+        String mailSubject = mailSubjectElement.getText();
         indexSenderName = senderName.indexOf(nameSender);
         if (indexSenderName == -1) {
             senderName.add(nameSender);
@@ -84,14 +83,12 @@ public class MailAnalysis {
     }
 
     public void performMailAnalysis() {
-        SenderCounter currentSenderCounter;
         loadMailProgram();
         parseMailList();
         for (SenderCounter senderCounter : senderCounters) {
-            currentSenderCounter = senderCounter;
-            System.out.println("Отправитель - " + currentSenderCounter.getFrom());
-            System.out.println("Писем о вакансиях - " + currentSenderCounter.getJobListCount());
-            System.out.println("Других писем - " + currentSenderCounter.getOtherListCount());
+            System.out.println("Отправитель - " + senderCounter.getFrom());
+            System.out.println("Писем о вакансиях - " + senderCounter.getJobListCount());
+            System.out.println("Других писем - " + senderCounter.getOtherListCount());
             System.out.println("------------------------------------------------------------------");
         }
     }
